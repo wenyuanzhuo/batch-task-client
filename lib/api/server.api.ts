4705ee04b954d7195d2c0805ee33e87222e40ab5
wenyuanzhuo <775416111@qq.com>
2019-03-15 16:17:30 +0800

    [done] success story

--- lib/api/server.api.ts
+++ lib/api/server.api.ts
@@ -11,6 +11,16 @@ export function queryPresaleOrderStatus(orderId): Observable<any> {
   }).then(response => response.data))
 }
 
+export function setPresaleDropSuccess(orderId): Observable<any>  {
+  return from(request.getInstance().request({
+    method: 'post',
+    url: serverApi.setPresaleDropSuccess,
+    data: helper.buildQuery({
+      order_id: orderId,
+    }),
+  }).then(response => response.data))
+}
+
 export function setPresaleOrderStatusApi(orderId): Observable<any> {
   return from(request.getInstance().request({
     method: 'post',
--- lib/story/success.ts
+++ lib/story/success.ts
@@ -3,6 +3,7 @@ import { Observable, zip, of } from 'rxjs';
 import { concatMap } from 'rxjs/operators'
 import { logger } from '../services/logger'
 import { StoryRegister } from '../decorators/register'
+import * as serverApi from '../api/server.api'
 import * as reader from '../services/reader'
 import { appConfig } from '../config/global'
 import chalk from 'chalk'
@@ -19,25 +20,44 @@ export default class SuccessStory implements BaseStory {
   }
 
   readFile(): Promise<any> {
-    return Promise.resolve({
-      status: false,
-    });
-  }
-
-
-  execute(): Observable<any> {
     const filePath = path.resolve('resources/data.json')
     if (!fs.existsSync(filePath)) {
       throw new Error('找不到数据文件')
     }
+
     const str = fs.readFileSync(filePath, 'utf-8')
     const data = JSON.parse(str)
-    data.source = []
+    return Promise.resolve({ data, filePath });
+  }
 
-    fs.writeFileSync(filePath, JSON.stringify(data), 'utf8')
+  setSuccess({ data, filePath }): Promise<any> {
+    const { source = [] } = data
+    const arr = []
+    console.log(source, '-----')
+    source.forEach(item => {
+      const { order_id } = item
+      arr.push(serverApi.queryPresaleOrderStatus(order_id).toPromise())
+    });
+    Promise.all(arr).then((results) => {
+      console.log(results, '===')
+      for(const item in results) {
+        const orderId = results[item].order_id
+        const res = results[item]
+        console.log(res)
+        if(res.order_id && res.order_id === orderId && data.process[res.order_id].refundStatus && data.process[res.order_id].setStatus) {
+          serverApi.setPresaleDropSuccess(res.order_id)
+          data.process[res.order_id].successStatus = true
+          fs.writeFileSync(filePath, JSON.stringify(data), 'utf8')
+        }
+      }
+    })
+    return Promise.resolve(true)
+  }
+  execute(): Observable<any> {
     return of(Promise.resolve(logger.info([
       '>', 'Starting Loading...'
     ].join(' '))))
       .pipe(concatMap(this.readFile))
+      .pipe(concatMap(this.setSuccess))
   }
 }
